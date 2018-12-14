77c791eec35bec4048f7a09340e362549fd9d8a6
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-13 20:47:15 -1000

    Completes exercise

--- calculator.js
+++ calculator.js
@@ -5,61 +5,136 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+var calculatorModule = function () {
+    var memory = 0;
+    var total = 0;
+
+    var load = function (num) {
+        if (typeof num === 'number') {
+            total = num;
+            return num;
+        } else {
+            throw error;
+        }
+    }
+    var getTotal = function () {
+        if (typeof total === 'number') {
+            return total;
+        }
+    }
+
+    var add = function (num) {
+        if (typeof num === 'number') {
+            return total += num;
+        } else {
+            throw error;
+        }
+    }
+
+    var subtract = function (num) {
+        if (typeof num === 'number') {
+            return total -= num;
+        } else {
+            throw error;
+        }
+    }
+
+    var multiply = function (num) {
+        if (typeof num === 'number') {
+            return total *= num;
+        } else {
+            throw error;
+        }
+    }
+
+    var divide = function (num) {
+        if (typeof num === 'number') {
+            return total /= num;
+        } else {
+            throw error;
+        }
+    }
+
+    var recallMemory = function () {
+        return memory;
+    }
+
+    var saveMemory = function () {
+        memory = total;
+
+    }
+
+    var clearMemory = function (num) {
+        memory = 0;
+    }
+
+    return {
+        load,
+        getTotal,
+        add,
+        subtract,
+        multiply,
+        divide,
+        recallMemory,
+        saveMemory,
+        clearMemory
+    }
 
 
-  /**
-   * sets the `total` to the number passed in
-   * @param  { Number } x
-   * @return { Number }    current total
-   */
+};
+
+/**
+ * sets the `total` to the number passed in
+ * @param  { Number } x
+ * @return { Number }    current total
+ */
 
 
-  /**
-   * Return the value of `total`
-   * @return { Number }
-   */
+/**
+ * Return the value of `total`
+ * @return { Number }
+ */
 
 
-  /**
-   * Sums the value passed in with `total`
-   * @param { Number } x
-   */
+/**
+ * Sums the value passed in with `total`
+ * @param { Number } x
+ */
 
 
-  /**
-   * Subtracts the value passed in from `total`
-   * @param  { Number } x
-   */
+/**
+ * Subtracts the value passed in from `total`
+ * @param  { Number } x
+ */
 
 
-  /**
-   * Multiplies the value by `total`
-   * @param  { Number } x
-   */
+/**
+ * Multiplies the value by `total`
+ * @param  { Number } x
+ */
 
 
-  /**
-   * Divides the value passing in by `total`
-   * @param  { Number } x
-   */
+/**
+ * Divides the value passing in by `total`
+ * @param  { Number } x
+ */
 
 
-  /**
-   * Return the value stored at `memory`
-   * @return { Number }
-   */
+/**
+ * Return the value stored at `memory`
+ * @return { Number }
+ */
 
 
-  /**
-   * Stores the value of `total` to `memory`
-   */
+/**
+ * Stores the value of `total` to `memory`
+ */
 
 
-  /**
-   * Clear the value stored at `memory`
-   */
+/**
+ * Clear the value stored at `memory`
+ */
 
-  /**
-   * Validation
-   */
-
+/**
+ * Validation
+ */
